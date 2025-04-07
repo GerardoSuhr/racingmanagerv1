12e62e5a0af9b05bc9cc6ef173f9ef5419c92973
GerardoSuhr <gerardo.suhr@hotmail.com>
2025-04-07 14:49:56 +0200

    agrego variables de entorno locales

--- server/db.js
+++ server/db.js
@@ -1,13 +1,22 @@
 const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+const fs = require('fs');
+
+// Decide qué archivo .env cargar
+const envFile = fs.existsSync('.env.local')
+  ? '.env.local'
+  : process.env.NODE_ENV === 'production'
+  ? '.env.production'
+  : '.env.development';
+
+dotenv.config({ path: envFile });
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Rajonrondo9@@', // tu password de MySQL
-  database: 'racingmanagerV1',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 module.exports = pool;
